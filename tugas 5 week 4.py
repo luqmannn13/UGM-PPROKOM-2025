e87eb2847367c244d336d84b5f129811be6435cd
luqmannn13 <luqmanulfaaiq@mail.ugm.ac.id>
2025-09-10 12:38:55 +0700

    Add files via upload

---
+++ tugas 5 week 4.py
@@ -0,0 +1,33 @@
+Arya=int(input("Masukkan nilai Arya:"))
+Bayu=int(input("Masukkan nilai Bayu:"))
+Citra=int(input("Masukkan nilai Citra:"))
+Danu=int(input("Masukkan nilai Danu:"))
+Endah=int(input("Masukkan niai Endah:"))
+
+print("HASIL KELULUSAN")
+
+if Arya >= 70 :
+    print("Selamat Arya, anda dinyatakan lulus")
+else :
+    print("Maaf Arya, anda tidak dinyatakan lulus")
+
+if Bayu >= 70 :
+    print("Selamat Bayu, anda dinyatakan lulus")
+else :
+    print("Maaf Bayu, anda tidak dinyatakan lulus")
+
+if Citra >= 70 :
+    print("Selamat Citra, anda dinyatakan lulus")
+
+else : 
+    print("Maaf Citra, anda tidak dinyatakan lulus")
+
+if Danu >= 70 :
+    print("Selamat Danu, anda dinyatakan lulus")
+else : 
+    print("Maaf Danu, anda tidak dinyatakan lulus")
+
+if Endah >= 70 :
+    print("Selamat Endah, anda dinyatakan lulus")
+else :
+    print("Maaf Endah, anda tidak dinyatakan lulus")                
